85aca3f7299ff90c643a4ccf4dc5e3b16bd2906f
DarshanDattaNaik <darshandattan.ec20@rvce.edu.in>
2022-07-26 14:18:31 +0000

    **

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -50,7 +50,38 @@ async def test_seq_bug2(dut):
     dut.reset.value = 0
     await FallingEdge(dut.clk)
 
-    #sequence-101011 
+    #sequence-1011011
+    dut.inp_bit.value= 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value= 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value= 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value= 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value= 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value= 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value= 1
+    await FallingEdge(dut.clk)
+    cocotb.log.info(f'current state={int(dut.current_state.value)} seq_seen={dut.seq_seen.value} model current state={4} model seq_seen={1}')
+    assert dut.seq_seen.value == 4,"The sequnce was not detected with current state={A} and seq_seen={B} expected current_state={C} and seq_seen={D}".format(A=int(dut.current_state.value),B=dut.seq_seen.value,C=4,D=0b1)
+
+@cocotb.test()
+async def test_seq_bug3(dut):
+    """Test for seq detection """
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+
+    #sequence-101011
     dut.inp_bit.value= 1
     await FallingEdge(dut.clk)
     dut.inp_bit.value= 0
@@ -59,10 +90,9 @@ async def test_seq_bug2(dut):
     await FallingEdge(dut.clk)
     dut.inp_bit.value= 0
     await FallingEdge(dut.clk)
-    '''dut.inp_bit.value= 1
+    dut.inp_bit.value= 1
     await FallingEdge(dut.clk)
     dut.inp_bit.value= 1
-    await FallingEdge(dut.clk)'''
-    assert dut.current_state.value==0,"current state {A}".format(A=int(current_state.value))
-    #cocotb.log.info(f'current state={int(dut.current_state.value)} seq_seen={dut.seq_seen.value} model current state={4} model seq_seen={1}')
-    #assert dut.seq_seen.value == 1,"The sequnce was not detected with current state={A} and seq_seen={B} expected current_state={C} and seq_seen={D}".format(A=int(dut.current_state.value),B=dut.seq_seen.value,C=4,D=0b1)
+    await FallingEdge(dut.clk)
+    cocotb.log.info(f'current state={int(dut.current_state.value)} seq_seen={dut.seq_seen.value} model current state={4} model seq_seen={1}')
+    assert dut.seq_seen.value == 1,"The sequnce was not detected with current state={A} and seq_seen={B} expected current_state={C} and seq_seen={D}".format(A=int(dut.current_state.value),B=dut.seq_seen.value,C=4,D=0b1)
