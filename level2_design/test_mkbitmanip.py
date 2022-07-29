df7040e6bdf89fee7def875e3eba32cfee739ca0
DarshanDattaNaik <darshandattan.ec20@rvce.edu.in>
2022-07-29 15:26:14 +0000

    found the logic

--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -34,10 +34,55 @@ def run_test(dut):
 
     ######### CTB : Modify the test to expose the bug #############
     # input transaction
+    for i in range((2^32)-1):
+        mav_putvalue_src1 = i
+        for j in range((2^32)-1):
+            mav_putvalue_src2 = j
+            for k in range((2^32)-1):
+               mav_putvalue_src3 = k
+               mav_putvalue_instr = 0x40007033
+
+               # expected output from the model
+               expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+
+               # driving the input transaction
+               dut.mav_putvalue_src1.value = mav_putvalue_src1
+               dut.mav_putvalue_src2.value = mav_putvalue_src2
+               dut.mav_putvalue_src3.value = mav_putvalue_src3
+               dut.EN_mav_putvalue.value = 1
+               dut.mav_putvalue_instr.value = mav_putvalue_instr
+  
+               yield Timer(1) 
+
+               # obtaining the output
+               dut_output = dut.mav_putvalue.value
+
+               cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
+               cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
+    
+               # comparison
+               error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)} with src1= {hex(dut.mav_putvalue_src1.value)}  src2= {hex(dut.mav_putvalue_src2.value)}  src3= {hex(dut.mav_putvalue_src3.value)}'
+               assert dut_output == expected_mav_putvalue, error_message
+
+
+'''
+@cocotb.test()
+def run_test2(dut):
+
+    # clock
+    cocotb.fork(clock_gen(dut.CLK))
+
+    # reset
+    dut.RST_N.value <= 0
+    yield Timer(10) 
+    dut.RST_N.value <= 1
+
+    ######### CTB : Modify the test to expose the bug #############
+    # input transaction
     mav_putvalue_src1 = 0x5
-    mav_putvalue_src2 = 0x0
+    mav_putvalue_src2 = 0x2
     mav_putvalue_src3 = 0x0
-    mav_putvalue_instr = 0x101010B3
+    mav_putvalue_instr = 0x40006033 #101010B3
 
     # expected output from the model
     expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
@@ -60,3 +105,4 @@ def run_test(dut):
     # comparison
     error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
     assert dut_output == expected_mav_putvalue, error_message
+'''
