11eca0caafe719927faa53ad825c0bbd00622fda
DarshanDattaNaik <darshandattan.ec20@rvce.edu.in>
2022-07-30 11:45:35 +0000

    started editing readme file

--- level3_design/test_vedic8x8.py
+++ level3_design/test_vedic8x8.py
@@ -1,15 +1,19 @@
 import cocotb
 from cocotb.triggers import Timer
+import random
 
 @cocotb.test()
-async def test1_mux(dut):
-    """Test for mux2"""
-    A = 20
-    B = 255
-    dut.a.value = A
-    dut.b.value = B
-    await Timer(2, units='ns')
+async def test_multiplier(dut):
+    """Test for multiplication"""
+    for i in range(10):
+       A = random.randint(0,255)
+       B = random.randint(0,255)
+       dut.a.value = A
+       dut.b.value = B
+       await Timer(2, units='ns')
     
-    dut.log.info(f'a={A} b={B} model={A*B} DUT={int(dut.p.value)}')
-    assert dut.p.value == A*B, "Randomised test failed with: a={A} and a={B} and product p!={P}"#.format(
-    #A=s, B=dut.inp12.value, S=dut.sel.value,M=dut.out.value,E=dut.inp12.value)
+       dut.log.info(f'a={A} b={B} model={A*B} DUT={int(dut.p.value)}')
+       assert dut.p.value == A*B, "Randomised test failed with: a={A} and a={B} and product p!={P}"#.format(
+       #A=s, B=dut.inp12.value, S=dut.sel.value,M=dut.out.value,E=dut.inp12.value)      
+
+
